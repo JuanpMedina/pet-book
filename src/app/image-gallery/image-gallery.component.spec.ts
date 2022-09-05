47f82a9129dfd089ece1a07a12d12616936fa537
JuanpMedina <juampamedinadiaz2002@gmail.com>
2022-09-04 22:18:13 -0500

    Pruebas unitarias 3.0

--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,25 +1,70 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ImageService } from '../image.service';
 import { GalleryComponent } from './image-gallery.component';
+
+
+class  mockImageService extends ImageService{
+getImages(){
+  return  [    
+    { "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
+    { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
+    { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
+    { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" },
+    { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" }
+]  ;
+}
+}
 
 describe('ImageGalleryComponent', () => {
   let component: GalleryComponent;
-  let fixture: ComponentFixture<GalleryComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
-    })
-    .compileComponents();
-  }));
-
+  let service: mockImageService;
+  
   beforeEach(() => {
-    fixture = TestBed.createComponent(GalleryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  service = new mockImageService();
+  component = new GalleryComponent(service);
+  });
+
+  
+  afterEach(() => {
+    service = null;
+    component = null;
   });
 
   it('should create', () => {
+    
     expect(component).toBeTruthy();
   });
+
+  describe('btn click filterby=perro', () =>  {
+   it('Todas las imagenes filtradas son perro', () => {
+      component.filterBy = 'perro';
+      component.ngOnChanges();
+     
+     for (let imagen of (component.allImages.filter(
+       (object)=> object.brand === component.filterBy )) 
+         ){
+      expect(imagen.brand).toBe('perro'); 
+     }
+  });
+});
+describe('btn click filterby=perro', () =>  {
+  it('Todas las imagenes filtradas son gato', () => {
+    component.filterBy = 'gato';
+
+    for (let imagen of (component.allImages.filter(
+      (object)=> object.brand === component.filterBy )) 
+    ){
+      expect(imagen.brand).toBe("gato"); 
+  } 
+ });
+});
+
+ xit('El Filtro perro debe corresponder al nombre del  boton Perro', () => {
+  component.ngOnChanges();
+  for (let imagen of component.allImages ){
+   expect(imagen.brand).toBe("gato"); 
+  }
+});
 });
